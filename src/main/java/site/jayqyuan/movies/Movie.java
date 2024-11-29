cbdb48c2d7cf354750dcce0b11159bc2d820ee56
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2024-11-29 16:37:34 -0500

    updated Movie class to match db

--- src/main/java/site/jayqyuan/movies/Movie.java
+++ src/main/java/site/jayqyuan/movies/Movie.java
@@ -1,7 +1,29 @@
 package site.jayqyuan.movies;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
 
 @Document(collation = "movies")
 public class Movie {
+    @Id
+    private ObjectId id;
+
+    private String imdbId;
+
+    private String title;
+
+    private String releaseDate;
+
+    private String trailerLink;
+
+    private String poster;
+
+    private List<String> genre;
+
+    private List<String> backdrops;
+
+
 }
