1ede294214e0ea4baefd8a9b3c6347a2994917cb
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2024-11-29 17:14:59 -0500

    created Review class

--- src/main/java/site/jayqyuan/movies/Review.java
+++ src/main/java/site/jayqyuan/movies/Review.java
@@ -7,29 +7,11 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
-
-@Document(collection = "movies")
+@Document(collection = "reviews")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Movie {
+public class Review {
     @Id
     private ObjectId id;
-
-    private String imdbId;
-
-    private String title;
-
-    private String releaseDate;
-
-    private String trailerLink;
-
-    private String poster;
-
-    private List<String> genre;
-
-    private List<String> backdrops;
-
-    private List<Review> reviewIds;
 }
