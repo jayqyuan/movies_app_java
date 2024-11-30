5167c2d822a30cc62b027be0d60c7d30367ea31c
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2024-11-30 17:05:36 -0500

    Created method for getting a single movie
    created API pathway for single movies in MovieController class, defined method in movieService Class

--- src/main/java/site/jayqyuan/movies/MovieController.java
+++ src/main/java/site/jayqyuan/movies/MovieController.java
@@ -1,13 +1,16 @@
 package site.jayqyuan.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 // purpose of a controller is just to get a request form the user and returning a response
 @RestController //indicate this class is a REST API controller
@@ -22,4 +25,9 @@ public class MovieController {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 
+    //retrieve a single movie
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id){
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(id), HttpStatus.OK);
+    }
 }
--- src/main/java/site/jayqyuan/movies/MovieService.java
+++ src/main/java/site/jayqyuan/movies/MovieService.java
@@ -1,8 +1,11 @@
 package site.jayqyuan.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
+import java.util.Optional;
 
 //The service class is where most of the business logics will go for backend
 //Service class uses the repository class to talk to the database and return to the API layer
@@ -15,5 +18,10 @@ public class MovieService {
     // This method retrieves all the movies from database where @return a list of all movies
     public List<Movie> allMovies(){
         return movieRepository.findAll();
+    }
+
+    //Optional because it could potentially return Null
+    public Optional<Movie> singleMovie(ObjectId id){
+        return movieRepository.findById(id);
     }
 }
