fec5d5c83a5d91f8fc4de8c92bbb15baf0d9469b
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2024-11-29 20:05:57 -0500

    connected to MOngodb and confirmed connection with GET request

--- src/main/java/site/jayqyuan/movies/MovieController.java
+++ src/main/java/site/jayqyuan/movies/MovieController.java
@@ -1,20 +1,24 @@
 package site.jayqyuan.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController //indicate this class is a REST API controller
 @RequestMapping("/api/v1/movies") //Maps all requests starting with "/api/v1/movies"
 public class MovieController {
     //handle GET requests
-
+    @Autowired
+    private MovieService movieService;
     //GetMapping Maps HTTP Get requests
     @GetMapping
-    public ResponseEntity<String> allMovies(){
-        return new ResponseEntity<String>("All Movies", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 
 }
