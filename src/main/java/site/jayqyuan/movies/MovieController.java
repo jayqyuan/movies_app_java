36f6ba481cbc5ff70015dc485eb7f142c4af6ab6
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2024-12-01 21:54:56 -0500

    reconfig to bypass CORS

--- src/main/java/site/jayqyuan/movies/MovieController.java
+++ src/main/java/site/jayqyuan/movies/MovieController.java
@@ -4,10 +4,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +12,7 @@ import java.util.Optional;
 // purpose of a controller is just to get a request form the user and returning a response
 @RestController //indicate this class is a REST API controller
 @RequestMapping("/api/v1/movies") //Maps all requests starting with "/api/v1/movies"
+@CrossOrigin(origins = "*")
 public class MovieController {
     //handle GET requests
     @Autowired
