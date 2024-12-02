d68cd1f12698ba01f7342508c791d55d0ecc646e
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2024-12-01 19:15:29 -0500

    fixed connection for POST request in creating new reviews

--- src/main/java/site/jayqyuan/movies/ReviewService.java
+++ src/main/java/site/jayqyuan/movies/ReviewService.java
@@ -6,9 +6,11 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class ReviewService {
-    private ReviewRepository reviewRepository;
+    @Autowired
+    private ReviewRepository repository;
 
     //MongoTemplate dependency is a more flexible way to interact with mongoDB compared to repository
     @Autowired
@@ -16,14 +18,14 @@ public class ReviewService {
     //locate the movie with imdbId to and use the reviewBody to create review for the movie
     public Review createReview(String reviewBody, String imdbId){
         //step 1 create review with given reviewBody and insert into the review collection
-        Review review = reviewRepository.insert(new Review(reviewBody));
+        Review review = repository.insert(new Review(reviewBody));
 
         //step 3: use MongoTemplate to locate movie and associate the review with it
         mongoTemplate.update(Movie.class)// specify the entity type we will be updating (Movie class)
                 .matching( //define criteria for selecting the movie to update.
                         Criteria.where("imdbId").is(imdbId) // locate movie by the imdbId
                 )
-                .apply(new Update().push("reviewIds").value(review)) //add the review's ID to the movie's reviewIDs array field
+                .apply(new Update().push("reviewIds").value(review.getId())) //add the review's ID to the movie's reviewIDs array field
                 .first(); //apply update to the first matching document (or only one in this case)
 
         //return the created Review object for further use or confirmation
